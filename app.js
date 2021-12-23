8114ee6d7a535643897c716280862895262bf227
Ramzt3 <89145511+Ramzt3@users.noreply.github.com>
2021-12-23 12:54:05 +0700

    add website

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 // const chalk = require('chalk'); // change color, but it's bug right now
 const debug = require('debug')('app'); // cmd => set DEBUG=* & node app.js
 const morgan = require('morgan'); // check info of incomming request
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (req,res) => {
     res.send('Hello lol luu no');
